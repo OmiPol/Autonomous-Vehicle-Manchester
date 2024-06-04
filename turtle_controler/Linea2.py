ff61125044caa17f7b5dd3b5cef5711f16fda2b7
Ivndxx <ivanuriel34@hotmail.com>
2024-06-04 10:42:24 -0600

    Creuce de zebra
    Holiiii, es el cruce de zebra con seguidor de linea en la Linea2 y cambios al control

--- turtle_controler/Linea2.py
+++ turtle_controler/Linea2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import cv2, rclpy, time
 import numpy as np
+import time
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
@@ -9,12 +10,14 @@ from std_msgs.msg import Float32, Int32
 
 class LineDetect(Node):
    def __init__(self):
-       super().__init__("Line_Detection")
+       super().__init__("image_processing")
        self.get_logger().info("Cam Subscriber Started...!!!")
        self.vid = None
        self.sub = self.create_subscription(Image, '/video_source/raw',self.camera_callback,10)
-       self.pub = self.create_publisher(Int32, '/line_error', 10)
-       self.timer = self.create_timer(0.05, self.timer_callback)
+       self.pub1 = self.create_publisher(Int32, '/line_error', 10)
+       self.pub2 = self.create_publisher(Int32, '/zebra', 10)
+       self.timer1 = self.create_timer(0.05, self.timer_callback_zebras)
+       self.timer2 = self.create_timer(0.05, self.timer_callback_line)
        self.width, self.height = 320,180
        self.color_img = np.uint16((self.width, self.height, 3))
        self.gray_img = np.uint16((self.width, self.height))
@@ -25,7 +28,53 @@ class LineDetect(Node):
    def camera_callback(self,msg):
        self.vid = self.bridge.imgmsg_to_cv2(msg, "bgr8") #bgr8
 
-   def timer_callback(self):
+   def timer_callback_zebras(self): #Callback de deteccion de zebra
+    msg = Int32()
+    if self.vid is not None:
+        self.gray_img = cv2.cvtColor(self.vid, cv2.COLOR_BGR2GRAY)
+        roi = self.gray_img[(self.height)*3//4:, self.width//4:3*self.width//2] # ROI en el tercio medio de la imagen
+        blurred = cv2.GaussianBlur(roi, (5, 5), 0)
+        _, mask = cv2.threshold(blurred, 80, 255, cv2.THRESH_BINARY_INV) # threshold for black color
+
+        #centro del roi
+        cx = self.width // 2
+        cy = self.height // 3
+
+        # find contours of the mask
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+        centroids = []
+
+        for contour in contours:
+            # get centroid of the contour
+            M = cv2.moments(contour)
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                centroids.append((cX, cY))
+
+                cv2.circle(mask, (cX, cY), 5, (0, 255, 0), -1)
+            
+        
+        # sort centroids by their y-coordinate
+        centroids.sort(key=lambda x: x[1])
+
+        # check if there are three centroids in parallel
+        if len(centroids) >= 4 and len(centroids) <= 7:
+            for i in range(len(centroids) - 2):
+                if abs(centroids[i][1] - centroids[i+1][1]) <= 5 and abs(centroids[i][1] - centroids[i+2][1]) <= 5:
+                    print("CRUCE DE ZEBRA")
+                    msg.data = 0
+                    self.pub2.publish(msg)
+        else:
+            msg.data = 1
+            self.pub2.publish(msg)
+
+        cv2.imshow("Cruces", mask)
+        cv2.imshow("Full", self.vid)
+        cv2.waitKey(1)
+
+   def timer_callback_line(self):
     msg = Int32()
     if self.vid is not None:
         self.gray_img = cv2.cvtColor(self.vid, cv2.COLOR_BGR2GRAY)
@@ -76,7 +125,7 @@ class LineDetect(Node):
 
         msg.data = int(error)
                 
-        self.pub.publish(msg)
+        self.pub1.publish(msg)
         #self.get_logger().info(f"Error mínimo: {error} píxeles")
         cv2.imshow("Full", self.vid)
         cv2.imshow("Puzzlebot", mask)
@@ -93,4 +142,5 @@ def main(args=None):
 
 if __name__ == "__main__":
    main()
+
 
